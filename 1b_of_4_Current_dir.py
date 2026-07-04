9f14a1a5f583decf47c206f5801d9a7ce887d53d
Goodness Chidirim Nnamdi <goodness.nnamdi001@gmail.com>
2026-07-04 17:25:22 +0100

    June 11th

---
+++ 1b_of_4_Current_dir.py
@@ -0,0 +1,4 @@
+import os
+
+current_directory = os.getcwd()
+print("Current Working Directory is:", current_directory)
--- 2_Current_dir.py
+++
@@ -1,4 +0,0 @@
-import os
-
-current_directory = os.getcwd()
-print("Current Working Directory is:", current_directory)
